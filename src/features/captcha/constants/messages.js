b4637e8e4f8adf10243550470fa7045a6f13d532
김명규 <adrian@gimmyeong-gyus-MacBook-Pro.local>
2026-02-23 16:05:10 +0900

    Adjust delayed message offsets

--- src/features/captcha/constants/messages.js
+++ src/features/captcha/constants/messages.js
@@ -6,37 +6,37 @@ export const PREVIEW_DELAYED_MESSAGES = [
   },
   {
     text: "싱싱한 [%const CAPTCHA_TARGETS]는 지금 당장 AI 가 학습을 실천합니다.",
-    offsetX: 12,
+    offsetX: 0,
     offsetY: 2,
   },
   {
     text: "어때요?",
-    offsetX: -8,
+    offsetX: 0,
     offsetY: 6,
   },
   {
     text: "연상력과 상상력이 담긴 신선한 [%const CAPTCHA_TARGETS]",
-    offsetX: -8,
+    offsetX: 0,
     offsetY: 6,
   },
   {
     text: "AI가 이해할 수 있을까요?",
-    offsetX: -8,
+    offsetX: 0,
     offsetY: 6,
   },
   {
     text: "이제 우리는 [데이터]를 우리가 원하는 만큼 AI에게 건네줄 차례입니다.",
-    offsetX: 6,
+    offsetX: 0,
     offsetY: 0,
   },
   {
     text: "더 이상 일방적으로 AI 에게 우리의 데이터를 탈취당하지 말아요.",
-    offsetX: -10,
+    offsetX: 0,
     offsetY: 4,
   },
   {
     text: "그동안 우리가 건넨 갓 건져올린 [데이터]들을 한 번 구경해봐요.",
-    offsetX: 4,
+    offsetX: 0,
     offsetY: 8,
   },
   {
